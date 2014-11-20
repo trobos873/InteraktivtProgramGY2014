f89e3434a7950883b81d16150f72946e8fb60637
trobos873 <oskar@trobobs.net>
2014-11-20 14:37:59 +0100

    HEI

--- InteraktivtProgramGY14/InteraktivtProgramGY14/Game1.cs
+++ InteraktivtProgramGY14/InteraktivtProgramGY14/Game1.cs
@@ -82,7 +82,7 @@ namespace InteraktivtProgramGY14
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
 
